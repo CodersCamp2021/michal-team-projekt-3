324cec0e78af4b214abc093a943d0b5ea4820b6f
Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>
2022-03-20 16:14:49 +0100

    Add languages and accomodation types constants

--- src/constants.js
+++ src/constants.js
@@ -9,3 +9,18 @@ export const PAYMENT_METHOD = {
   CREDIT_CARD: 'credit-card',
   PAYING_ON_PLACE: 'paying_on_place',
 };
+
+export const LANGUAGE = {
+  ENGLISH: 'english',
+  POLISH: 'polish',
+  GERMAN: 'german',
+  SPANISH: 'spanish',
+  FRENCH: 'french',
+};
+
+export const ACCOMODATION_TYPE = {
+  HOTEL: 'hotel',
+  HOSTEL: 'hostel',
+  APARTMENT: 'apartment',
+  HOUSE: 'house',
+};
